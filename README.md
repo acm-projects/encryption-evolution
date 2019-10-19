8b80f61c3e4091b0051726d78a8aef6867a9160c
aditi-prabhu <arp180009@utdallas.edu>
2019-10-19 15:19:33 -0500

    Finished encryption tutorial, needs label for key

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # encryption-evolution
-2D game that teaches people the evolution of encryption, from simple ciphers to modern encryption algorithms!
+2D game that teaches people the evolution of encryption, from simple ciphers to modern encryption algorithms
 
 ## MVP
 - A starting main menu with selectable levels
@@ -20,19 +20,11 @@
 
 ## Resources
 - [Git Bash for Windows](https://gitforwindows.org)
-  - Mac users can just use the console
-  - [Git Cheat Sheet](https://github.github.com/training-kit/downloads/github-git-cheat-sheet.pdf)
 - Cyptography:
   - [Cipher Options](http://practicalcryptography.com/ciphers/)
-    - A good website with a lot of ciphers, an introduction to each, and their algorithms.
-  - [Alt. Cipher Website](http://rumkin.com/tools/cipher/)
-    - Alternate cipher website
   - [Fisher-Yates Shuffle Algorithm](https://exceptionnotfound.net/understanding-the-fisher-yates-card-shuffling-algorithm/)
-    - An extra option not included in the above website.
-  - [Crash Course](https://www.youtube.com/watch?v=jhXCTbFnK8o&vl=en)
-    - Intro to cyptography feat. CrashCourse on youtube. Also, a good example on how to teach others about cryptography and ciphers.
   - [Coursera: Cryptography](https://www.coursera.org/learn/crypto?action=enroll)
-    - Free course that officially starts after Sept. 30, but I'm pretty sure you can view all the material right now. A little late to be useful, but maybe for future reference if you want to continue learning more about cryptography on your own.
+  - [Crash Course](https://www.youtube.com/watch?v=jhXCTbFnK8o&vl=en)
 - Free 2D Assets 
   - [GameDeveloperStudio](https://www.gamedeveloperstudio.com/index.php)
   - [Itch.io](https://itch.io/game-assets/free/tag-2d)
@@ -41,9 +33,8 @@
   - Open source (extendable through C++), built-in IDE, cross platform (including 3DS!), reportedly easy version control with Git, language: GDScript which is similar to Python or you can use C++, light-weight
   - Tutorials:
     - [Official Godot Documentation : Getting Started](https://docs.godotengine.org/en/3.1/getting_started/step_by_step/index.html)
-    - [Programming in GDScript Intro](https://www.gamefromscratch.com/page/Godot-3-Tutorial-GDScript-Programming-101.aspx)
+    - [GDScript Intro](https://www.gamefromscratch.com/page/Godot-3-Tutorial-GDScript-Programming-101.aspx)
     - [Godot Engine 3 - Platform Game Tutorial](https://www.youtube.com/watch?v=wETY5_9kFtA) 
-      - A tutorial on how to create a simple 2D platformer starting at the first time you open Godot. Good for learning how to navigate the environment.
 - However, feel free to look around at other game engines
   - [Cocos2DX](https://cocos2d-x.org/)
     - Open source, cross platform, language: Lua, lots of 3rd party assets
---
+++ TutorialSpinner2.gd
@@ -0,0 +1,210 @@
+extends Area2D
+
+onready var wheel = get_node("OuterWheel") # variable containing the outer wheel sprite that can rotate
+var input_angle = 0 # the angle that the user inputs based on the key that they input
+var num_letters_pressed = 0 # how many letters have been entered, AKA how many buttons have been pressed
+var label_dict # dictionary from number of letters pressed to label to be populated
+var encryptor = null # encryptor object to call for the decrypted message variable
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	encryptor = load("res://CaesarEncryptor2.gd").new()
+	set_physics_process(true)
+	set_process_input(true) 
+	wheel.set_meta("_edit_lock_", true)
+	label_dict = {0: "Blank1", 1: "Blank2", 2: "Blank3", 3: "Blank4", 4: "Blank5", 5: "Blank6",
+	 6: "Blank7", 7: "Blank8", 8: "Blank9", 9: "Blank10", 10: "Blank11", 11: "Blank12", 12: "Blank13",
+	 13: "Blank14", 14: "Blank15", 15: "Blank16", 16: "Blank17", 17: "Blank18", 18: "Blank19", 19: "Blank20",
+	 20: "Blank21", 21: "Blank22", 22: "Blank23"}
+
+# capturing mouse click on spinner
+func _on_Spinner_input_event(viewport, event, shape_idx):
+	if event.is_action("lmb"):
+		wheel.set_meta("_edit_lock_", true)
+
+# sets the rotation of the spinner to the required offset entered by the user
+func _on_GoButton_pressed():
+	input_angle = get_tree().get_root().get_node("CaesarCipher").find_node("ShiftAmountField").get_value()
+	input_angle = ((2*PI) / 26) * input_angle
+	wheel.set_rotation(input_angle)
+
+# deletes the last entered letter by decrementing num_letters_pressed and returning the corresponding text field to "_"
+func _on_Backspace_pressed():
+	if num_letters_pressed > 10: # TODO Use variable size instead of hard coding
+		num_letters_pressed = 10
+	num_letters_pressed = num_letters_pressed - 1
+	get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank10")).set_text("_")
+
+func _process(delta):
+	_check_Encrypted()
+
+# checks if entered message is correctly encrypted
+# called when the last letter of the word (D) is entered (see line 151) (should optimize later)
+func _check_Encrypted():
+	var encryptedMessage = self.encryptor._getEncryptedMessage()
+	
+	var enteredMessage = ""
+	
+	for i in range(label_dict.size()):       # combines every "Blank"'s text into one string
+		var theBlank = label_dict.get(i)
+		var theLetter = get_tree().get_root().get_node("CaesarCipher").find_node(theBlank).text
+		enteredMessage += theLetter
+	
+	if (enteredMessage == encryptedMessage):
+		get_tree().get_root().get_node("CaesarCipher").find_node("NextArrow").visible = true # Shows green arrow
+
+# all of these funtions allow the letter buttons to function. They change the appropriate
+# text field to the button value and then increment the couter of the number of buttons pressed.
+func _on_A_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("A");
+		num_letters_pressed = num_letters_pressed + 1
+
+func _on_N_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("N");
+		num_letters_pressed = num_letters_pressed + 1
+
+func _on_R_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("R");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_S_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("S");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_T_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("T");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_U_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("U");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_W_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("W");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_X_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("X");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_Y_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("Y");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_Z_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("Z");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_O_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("O");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_P_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("P");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_Q_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("Q");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_V_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("V");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_B_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("B");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_C_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("C");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_D_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("D");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_E_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("E");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_F_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("F");
+		num_letters_pressed = num_letters_pressed + 1
+		_check_Encrypted() # Checks if entered message is correctly encrypted
+
+
+func _on_G_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("G");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_H_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("H");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_I_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("I");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_J_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("J");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_K_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("K");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_L_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("L");
+		num_letters_pressed = num_letters_pressed + 1
+
+
+func _on_M_pressed():
+	if wheel.get_meta("_edit_lock_") == true:
+		get_tree().get_root().get_node("CaesarCipher").find_node(label_dict.get(num_letters_pressed, "Blank7")).set_text("M");
+		num_letters_pressed = num_letters_pressed + 1
