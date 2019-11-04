4b034a78ef0f331989fdb38f61da03d005230360
edenfharris <edenfharris@gmail.com>
2019-11-04 13:06:36 -0600

    All of the transitions now work and assets have been adjusted for consistency.

--- Scenes/Playfair/Scripts/PlayfairHowTo.gd
+++ Scenes/Playfair/Scripts/PlayfairHowTo.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var dialogue = ["In order to use the playfair cipher, we need a keyword. For this tutorial we will use \"keyword\" as our keyword.",
+var dialogue = ["That's it! This is the Playfair cipher, which was used heavily in World War I and World War II. In order to use the Playfair cipher, we need a keyword. For this tutorial we will use \"keyword\" as our keyword.",
 "\"Keyword\" does not have repeating letters, but if it did we would delete the repeated letters.",
 "As shown on the left, we next write the keyword on a 5x5 grid followed by all other letters of the alphabet not already present in the keyword.", 
-"We will next examine the message that we want to encrypt. For this example, we will ecrypt the word \"MESSAGE\".",
+"We will next examine the message that we want to encrypt. For this example, we will ecrypt the word \"MESSAGE.\"",
 "If the message has repeating letters, replace the second in the pair with an 'x'. If there is an odd number of letters, add an 'x' at the end.",
 "Begin encryption by splitting up the message into groups of two letters.", 
 "Start with the first pair. Find them on the grid on the left.", 
@@ -15,19 +15,24 @@ var dialogue = ["In order to use the playfair cipher, we need a keyword. For thi
 "SX becomes QZ",
 "AG becomes DH",
 "EX becomes WU",
-"Encrypting is now finished! Decrypting a message will follow a similar process in reverse. Press continue to move on."]
+"Encrypting is now finished! Decrypting a message will follow a similar process in reverse. Press continue to try decrypting a message."]
 
 var index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").set_bbcode(dialogue[index])
+	get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").set_visible_characters(0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _on_ContinueButton_pressed():
+	if get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").get_visible_characters() > get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").get_total_character_count():
+			if index < dialogue.size()-1:
+				index += 1
+				get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").set_bbcode(dialogue[index])
+				get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").set_visible_characters(0)
+			else:
+				get_tree().change_scene("res://Scenes/Playfair/PFDecryptTutorial.tscn")
 	if index < dialogue.size():
 		if index == 4:
 			get_tree().get_root().get_node("PlayfairAsset").find_node("Message").set_text("Encrypt: MESXAGEX")
@@ -52,5 +57,12 @@ func _on_ContinueButton_pressed():
 			get_tree().get_root().get_node("PlayfairAsset").find_node("EncryptedMessage").set_text("NKQZDHWU")
 		if index == 15:
 			get_tree().get_root().get_node("PlayfairAsset").find_node("LetterBoxOutline4").hide()
-		get_tree().get_root().get_node("PlayfairAsset").find_node("TextBox").set_text(dialogue[index])
-		index = index + 1
+		#get_tree().get_root().get_node("PlayfairAsset").find_node("TextBox").set_text(dialogue[index])
+		#index = index + 1
+	else:
+		get_tree().change_scene("res://Scenes/Playfair/PFDecryptTutorial.tscn")
+
+
+# Auto-scrolls text
+func _on_Timer_timeout():
+	get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").set_visible_characters(get_tree().get_root().get_node("PlayfairAsset").find_node("RichTextLabel").get_visible_characters()+1)
